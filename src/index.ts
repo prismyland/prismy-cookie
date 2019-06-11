9473c81a90159ef8aa1756f273460e32a301e6dc
Junyoung Choi <fluke8259@gmail.com>
2019-06-11 09:22:31 +0900

    Fix return type of createCookieSelector

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import {
   DefaultCookieOptions,
   InternalCookieStore
 } from './InternalCookieStore'
+import { CookieStore } from './CookieStore'
 export * from './CookieStore'
 export * from './MockCookieStore'
 export { DefaultCookieOptions }
 
 export function createCookieSelector(
   options: DefaultCookieOptions = {}
-): Selector<any> {
+): Selector<CookieStore> {
   return (req, res) => {
     return new InternalCookieStore(req, res, options)
   }
