0439151413e5bdc49a115acb5b47aa305c801003
Junyoung Choi <fluke8259@gmail.com>
2019-07-25 17:56:49 +0900

    Improve test of MockCookieStore
    - Fix typo
    - Add test for multiple setter calls

--- src/specs/MockCookieStore.spec.ts
+++ src/specs/MockCookieStore.spec.ts
@@ -11,9 +11,20 @@ test('MockCookieStore#constructor accepts default cookies', t => {
   })
 })
 
-test('MockCookieStore#set stores new coookies', t => {
+test('MockCookieStore#set stores new cookies', t => {
   const cookieStore = new MockCookieStore()
   cookieStore.set(['message', 'Hello, World!'])
 
   t.deepEqual(cookieStore.newCookies, [['message', 'Hello, World!']])
+})
+
+test('MockCookieStore#set append cookies to newCookies if there are preset cookies', t => {
+  const cookieStore = new MockCookieStore()
+  cookieStore.set(['message', 'Hello, World!'])
+  cookieStore.set(['message2', 'Hello, World2!'])
+
+  t.deepEqual(cookieStore.newCookies, [
+    ['message', 'Hello, World!'],
+    ['message2', 'Hello, World2!']
+  ])
 })
