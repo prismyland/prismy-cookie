b40ff4b6422772afa7033d75141235f5cb5c15e6
Junyoung Choi <fluke8259@gmail.com>
2019-08-09 18:37:01 +0900

    Renew api

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,57 @@
-import { createInjectDecorators, Selector } from 'prismy'
-import { InternalCookieStore } from './InternalCookieStore'
-import { CookieStore } from './CookieStore'
-export * from './CookieStore'
-export * from './MockCookieStore'
+import {
+  parse,
+  serialize,
+  CookieParseOptions,
+  CookieSerializeOptions
+} from 'cookie'
+import {
+  SyncSelector,
+  headersSelector,
+  ResponseObject,
+  updateHeaders
+} from 'prismy'
 
-const cookieStoreSymbol = Symbol('prismy-cookie')
+export type ParsedCookies = { [key: string]: string }
 
-export const cookieSelector: Selector<CookieStore> = context => {
-  let cookieStore = context[cookieStoreSymbol]
-  if (cookieStore == null) {
-    context[cookieStoreSymbol] = cookieStore = new InternalCookieStore(context)
+export function createCookiesSelector(
+  options: CookieParseOptions = {}
+): SyncSelector<ParsedCookies> {
+  const cookiesSymbol = Symbol('prismy-cookie')
+  return context => {
+    let parsedCookie = context[cookiesSymbol] as ParsedCookies | undefined
+    if (parsedCookie == null) {
+      context[cookiesSymbol] = parsedCookie = parse(
+        headersSelector(context).cookie || '',
+        options
+      )
+    }
+    return parsedCookie
   }
-  return cookieStore
 }
 
-export function Cookie() {
-  return createInjectDecorators(cookieSelector)
+export function setCookie<B>(
+  resObject: ResponseObject<B>,
+  ...cookies: Array<[string, string, CookieSerializeOptions] | [string, string]>
+): ResponseObject<B> {
+  return updateHeaders(resObject, {
+    'set-cookie': serializeCookies(...cookies)
+  })
+}
+
+export function appendCookie<B>(
+  resObject: ResponseObject<B>,
+  ...cookies: Array<[string, string, CookieSerializeOptions] | [string, string]>
+): ResponseObject<B> {
+  const presetCookieHeaderList =
+    (resObject.headers['set-cookie'] as string[] | undefined) || []
+
+  return updateHeaders(resObject, {
+    'set-cookie': [...presetCookieHeaderList, ...serializeCookies(...cookies)]
+  })
+}
+
+function serializeCookies(
+  ...cookies: Array<[string, string, CookieSerializeOptions] | [string, string]>
+) {
+  return cookies.map(([key, value, options]) => serialize(key, value, options))
 }
